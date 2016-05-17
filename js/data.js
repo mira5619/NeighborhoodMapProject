96a4a1893aabab8eba3d36e71da73bf2aebdfb6a
Margarita Ivanova <ivanova5619@abv.bg>
2016-05-17 18:16:01 +0100

    fixed indentation in data.js by running it through a beautifier http://www.cleancss.com/html-beautify/ as advised in my review

--- js/data.js
+++ js/data.js
@@ -1,38 +1,31 @@
-
-var mylist = [
-  {
-  name: "Portchester Castle",
-  location: "Church Road, Portchester, Hampshire PO16 9QW, United Kingdom",
-  descrString: "The Spinnaker Tower is a 170-metre landmark tower in Portsmouth, England, UK.",
-  lat: 50.837415,
-  lng: -1.114906
-  },
-  {
-  name: "Emirates Spinnaker Tower",
-  location: "Portsmouth, Hampshire PO1 3TT",
-  descrString: "Commanding castle crucial to Solent defences for centuries, with audio-tours and picnic space.",
-  lat: 50.7960101,
-  lng: -1.1081846
-  },
-  {
-    name:"Southsea Castle",
+var mylist = [{
+    name: "Portchester Castle",
+    location: "Church Road, Portchester, Hampshire PO16 9QW, United Kingdom",
+    descrString: "The Spinnaker Tower is a 170-metre landmark tower in Portsmouth, England, UK.",
+    lat: 50.837415,
+    lng: -1.114906
+}, {
+    name: "Emirates Spinnaker Tower",
+    location: "Portsmouth, Hampshire PO1 3TT",
+    descrString: "Commanding castle crucial to Solent defences for centuries, with audio-tours and picnic space.",
+    lat: 50.7960101,
+    lng: -1.1081846
+}, {
+    name: "Southsea Castle",
     location: "Clarence Esplanade, Portsmouth PO5 3PA",
     descrString: "Built in 1544, the castle in Portsmouth was part of a series of fortifications constructed by Henry VIII around England's coasts to protect the country from invaders.",
     lat: 50.7781,
     lng: -1.08881
-  },
-  {
+}, {
     name: "University of Portsmouth",
     location: "Winston Churchill Ave, Portsmouth PO1 2UP, United Kingdom",
     descrString: 'British ales and pub grub in a small venue with a garden, photos of historic ships and a pool table',
     lat: 50.7957,
     lng: -1.0936
-  },
-  {
+}, {
     name: "No Man's Land Fort",
     location: "Canal Side, Gunwharf Quays, Portsmouth, PO1 3FH, United Kingdom",
     descrString: "The UK's most unusual hotel: a Victorian fort out at sea. No Man's Fort, located in the middle of the Solent, is just 1.4 miles from the Isle of Wight, and a mile from Portsmouth.",
     lat: 50.7333,
     lng: -1.1
-  }
-];
+}];
